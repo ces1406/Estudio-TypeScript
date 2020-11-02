dfe0a068226545f92428e009366bd91dce58bf4a
cesar montalvan <ces1406@gmail.com>
2020-11-02 19:36:01 -0300

    intial pruebas

---
+++ hola_mundo/app.js
@@ -0,0 +1,7 @@
+console.log("hola mundo tutu");
+function sum(n1, n2) {
+    return n1 + n2;
+}
+var n1 = 2;
+var n2 = 3.8;
+console.log(sum(n1, n2));
---
+++ hola_mundo/app.ts
@@ -0,0 +1,7 @@
+console.log("hola mundo tutu");
+function sum(n1,n2){
+    return n1+n2;
+}
+const n1 = 2;
+const n2 = 3.8;
+console.log(sum(n1,n2));
